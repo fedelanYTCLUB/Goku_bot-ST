d128c418107aa2fe504afe80dfc6b1e4f6a5a735
Ado <adonaylester85@gmail.com>
2025-04-21 17:05:26 -0600

    Update info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -13,12 +13,12 @@ let handler = async (m, { conn, usedPrefix, text, args, command }) => {
     let username = conn.getName(m.sender);
 
     let list = [{
-        displayName: "Wirk - Creador de Mai ✨",
+        displayName: "Wirk - Creador de Mai 🌹",
         vcard: `BEGIN:VCARD\nVERSION:3.0\nFN: Wirk - Bot Developer\nitem1.TEL;waid=50493732693:50493732693\nitem1.X-ABLabel:Número\nitem2.ADR:;;Honduras;;;;\nitem2.X-ABLabel:País\nEND:VCARD`,
     }];
 
-    const imageUrl = 'https://files.catbox.moe/k0kpaj.jpg';
-    const texto = `╭───────❀\n│ *Contacto del creador*\n╰───────❀\n\n• *Nombre:* Wirk\n• *Desde:* Honduras\n• *Creador de:* Mai\n\n_“El código es temporal, pero la creatividad... esa es eterna.”_\n\nPuedes contactarlo si tienes ideas, bugs o quieres apoyar el proyecto.`;
+    const imageUrl = 'https://qu.ax/VnCGk.jpg';
+    const texto = `╭───────❀\n│ *Contacto del creador*\n╰───────❀\n\n• *Nombre:* Wirk\n• *Desde:* Honduras\n• *Creador de:* Mai\n\n_“El código es temporal, pero la creatividad... esa es eterna.”_\n\nPuedes contactarlo si tienes ideas, bugs o quieres apoyar el proyecto..`;
 
     await conn.sendMessage(m.chat, {
         contacts: {
