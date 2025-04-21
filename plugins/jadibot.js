8117f8e86f771d12e38c6737e9081f3f7dc9528a
Ado <adonaylester85@gmail.com>
2025-04-21 16:28:09 -0600

    Update jadibot.js

--- plugins/jadibot.js
+++ plugins/jadibot.js
@@ -53,11 +53,11 @@ let handler = async (m, { conn: _envio, command, usedPrefix, args, text, isOwner
             const replyMessage = users.length === 0
                 ? `No hay Sub-Bots disponibles por el momento.`
                 : `
-❀ para ser un subbot usa el comando *#code*\n\n✧ *Sub-Bots conectados: ${users.length}*
-❒ total de comandos 303`;
+❀ Para ser un subbot usa el comando *#code*\n\n✧ *Sub-Bots conectados: ${users.length}*
+❒ Total de comandos 303`;
 
             const responseMessage = isMainBot
-                ? `*「✦」subbots activos*\n${replyMessage}`
+                ? `*「✦」Subbots activos 🌻*\n${replyMessage}`
                 : `${emoji} *ESTE ES UN SUB-BOT*\n${replyMessage}`;
 
             await _envio.sendMessage(m.chat, {text: responseMessage, mentions: _envio.parseMention(responseMessage)}, {quoted: m})
