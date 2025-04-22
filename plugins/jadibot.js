13f97ee0b248a502d8bc26f4d9ff0f1bc738716d
Ado <adonaylester85@gmail.com>
2025-04-22 17:10:56 -0600

    Update jadibot.js

--- plugins/jadibot.js
+++ plugins/jadibot.js
@@ -70,7 +70,7 @@ return resultado;
 const message = users.map((v, index) => `🙈 「 ${index + 1} 」\n📎 Wa.me/${v.user.jid.replace(/[^0-9]/g, '')}?text=${usedPrefix}estado\n👤 Usuario: ${v.user.name || 'Sub-Bot'}\n🕑 Conexión De: ${ v.uptime ? convertirMsADiasHorasMinutosSegundos(Date.now() - v.uptime) : 'Tiempo Desconocido 💀'}`).join('\n\n─────ೋღ 🌺 ღೋ─────\n\n');
 const replyMessage = message.length === 0 ? `No hay Sub-Bots disponible por el momento, verifique mas tarde.` : message;
 const totalUsers = users.length;
-const responseMessage = `🌸 🍀 *SUB-BOTS* ACTIVOS ACTUALMENTE ☑️ \n\n${emoji2} \`\`\`CADA USUARIO SUB-BOT USA SUS FUNCIONES COMO QUIERA, EL NÚMERO PRINCIPAL NO SE HACE RESPONSABLE DEL USO DEL MAL USO DE ELLA \`\`\`\n\n*🐻‍❄️ TOTAL SUB-BOTS CONECTADOS:* ${totalUsers || '0'}\n\n${replyMessage.trim()}`.trim();
+const responseMessage = `🌸 🍀 *SUB-BOTS* ACTIVOS ACTUALMENTE ☑️ \n\n${emoji2} \`\`\`CADA USUARIO SUB-BOT USA SUS FUNCIONES COMO QUIERA, EL NÚMERO PRINCIPAL NO SE HACE RESPONSABLE DEL USO DEL MAL USO DE ELLA \`\`\`\n\n*🐻‍❄️ TOTAL SUB-BOTS CONECTADOS:* ${totalUsers || '0'}\n\n${replyMessage.trim()}\nhttps://chat.whatsapp.com/KqkJwla1aq1LgaPiuFFtEY`.trim();
 await _envio.sendMessage(m.chat, {text: responseMessage, mentions: _envio.parseMention(responseMessage)}, {quoted: m})
 break   
 }}
