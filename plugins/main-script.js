8144ddb70ee75eb1d2f86702b50e59a0a02b23ae
Ado <adonaylester85@gmail.com>
2025-04-25 20:16:22 -0600

    Update main-script.js

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -1,28 +1,19 @@
-const handler = async (m, { conn, text, command }) => {
-  try {
-    // Verifica si el comando es 'script'
-    if (command === 'script') {
-      // Responde con el mensaje profesional y emojis
-      const message = `
-🌸 _*Mai Bot Sc*_  🌸
+const handler = async (m, { conn }) => {
+  const message = `
+🌸 _*Mai Bot Sc*_ 🌸
 
-```Repositorio OFC:```
+\`\`\`Repositorio OFC:\`\`\`
 https://github.com/Ado926/MaiBot
 
 > 🌻 Deja tu estrellita ayudaría mucho :D
 
-🔗 *Comunidad Oficial* https://chat.whatsapp.com/KqkJwla1aq1LgaPiuFFtEY
-      `;
-      // Envía el mensaje
-      await conn.reply(m.chat, message, m);
-    }
-  } catch (error) {
-    console.error("Error en el comando 'script':", error);
-    return m.reply(`⚠️ *Hubo un error al procesar tu solicitud.*`);
-  }
+🔗 *Comunidad Oficial:* https://chat.whatsapp.com/KqkJwla1aq1LgaPiuFFtEY
+  `;
+  await conn.reply(m.chat, message.trim(), m);
 };
 
-handler.command = handler.help = ['script'];
+handler.command = ['script'];
+handler.help = ['script'];
 handler.tags = ['info'];
 
 export default handler;
