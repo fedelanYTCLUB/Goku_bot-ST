0b7f7518a0404db70f0b5ee7266302600a4d4497
Ado <adonaylester85@gmail.com>
2025-04-27 09:09:52 -0600

    Update descargas-spotify.js

--- plugins/descargas-spotify.js
+++ plugins/descargas-spotify.js
@@ -147,7 +147,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
       `💎 *Nombre:* ${name}\n` +
       `🗣️ *Artista:* ${artist}\n` +
       `⏱️ *Duración:* ${duration}\n\n` +
-      `> @Barboza - Bot - 𝟢𝟨`,
+      `> Mai Bot`,
     contextInfo: {
       externalAdReply: {
         title: name,
