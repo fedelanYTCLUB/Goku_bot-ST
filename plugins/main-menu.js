59440dad8afd0befc5d0962b0980cf70e345fb32
Ado <adonaylester85@gmail.com>
2025-05-01 08:06:24 -0600

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn, args }) => {
-    let userId = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
-    let user = global.db.data.users[userId]
-    let name = conn.getName(userId)
-    let _uptime = process.uptime() * 1000
-    let uptime = clockString(_uptime)
-    let totalreg = Object.keys(global.db.data.users).length
-    let totalCommands = Object.values(global.plugins).filter((v) => v.help && v.tags).length
+let userId = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
+let user = global.db.data.users[userId]
+let name = conn.getName(userId)
+let _uptime = process.uptime() * 1000
+let uptime = clockString(_uptime)
+let totalreg = Object.keys(global.db.data.users).length
+let totalCommands = Object.values(global.plugins).filter((v) => v.help && v.tags).length
 
     let txt = `
 *ଘ(੭ˊ꒳ˋ)੭✧ New! Mira mi página de descargas:*
